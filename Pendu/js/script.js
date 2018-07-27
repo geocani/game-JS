efeb19396e8dd3597aa94c606044d31decf73a27
Canivet Geoffrey <geoffreycanivet@gmail.com>
2018-07-27 14:30:05 +0100

    maj

--- Pendu/js/script.js
+++ Pendu/js/script.js
@@ -12,28 +12,38 @@ for (let i = 0; i < motSecret.length; i++){
 let lettreManquantes = motSecret.length;
 let tentative = 0; // VARIABLE / nombre de tentative du joueur 
 // let checkLettre = motSecret.indexOf(essai) // VARIABLE / vérifie si la lettre est dans le mot / retourne l'index de la lettre ou -1
+let lettres = $('.lettres');
 
-while (lettreManquantes > 0) { //  BOUCLE
-    alert(tabTentative.join(" ")); // .join() / fusionne les element du tab dans une string (donnee separee par ())
-    // VERIFICATION
-    let proposition = prompt("devine une lettre."); // VARIABLE / lettre proposée par le joueur
-    if (proposition === null) { // CONDITION 
-        break; // Quitter la boucle
-    }
-    else if (proposition.length !== 1) {
-        alert("tu ne dois saisir qu'une seul lettre.");
-    }else{
-        // mettre a jour l'état du jeu
-        for (let j = 0; j < motSecret.length; j++) {
-            if (motSecret[j] === proposition) {
-                tabTentative[j] = proposition;
-                lettreManquantes--;
-                tentative++;
-                console.log(tentative)
-            }
-            if(lettreManquantes == 0) {
-                alert('gagné')
+
+
+
+$('.start').click( () =>{
+    while (lettreManquantes > 0) { //  BOUCLE
+        lettres.text(tabTentative.join(" "))
+        //alert(tabTentative.join(" ")); // .join() / fusionne les element du tab dans une string (donnee separee par ())
+        // VERIFICATION
+        
+
+
+        let proposition = prompt("devine une lettre."); // VARIABLE / lettre proposée par le joueur
+        if (proposition === null) { // CONDITION 
+            break; // Quitter la boucle
+        }
+        else if (proposition.length !== 1) {
+            alert("tu ne dois saisir qu'une seul lettre.");
+        }else{
+            // mettre a jour l'état du jeu
+            for (let j = 0; j < motSecret.length; j++) {
+                if (motSecret[j] === proposition) {
+                    tabTentative[j] = proposition;
+                    lettreManquantes--;
+                    tentative++;
+                    console.log(tentative)
+                }
+                if(lettreManquantes == 0) {
+                    alert('gagné')
+                }
             }
         }
     }
-}
+})
