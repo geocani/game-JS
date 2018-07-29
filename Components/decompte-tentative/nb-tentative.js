b1c28b3beca1d2c119b2a1e6feb0d1eaaf782b6e
Canivet Geoffrey <geoffreycanivet@gmail.com>
2018-07-29 23:36:06 +0100

    maj

--- Components/decompte-tentative/nb-tentative.js
+++
@@ -1,9 +0,0 @@
-let tentative = 10;
-
-let decompteTentative = () => {
-    tentative--
-    console.log('​decompteTentative -> tentative', tentative);
-    if (tentative <= 0){
-        alert('perdu')
-    }
-}
