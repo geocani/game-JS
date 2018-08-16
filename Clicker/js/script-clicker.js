4152f73a46f4ff4c23b7120a8ad86152681bdb33
Canivet Geoffrey <geoffreycanivet@gmail.com>
2018-08-16 16:10:46 +0100

    maj

---
+++ Clicker/js/script-clicker.js
@@ -0,0 +1,19 @@
+let clicker = $('.clicker');
+let contScore = $('.cont_score');
+let score = 0;
+
+let multiplicateur = 1;
+
+let augmenterMultiplicateur = () =>{
+    
+}
+
+
+
+
+
+
+clicker.click(()=>{
+    score++;
+    contScore.html(score);
+})
