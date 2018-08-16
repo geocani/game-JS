d062171625e7b66f8bcca4804795aa68424e35b4
Canivet Geoffrey <geoffreycanivet@gmail.com>
2018-08-16 19:21:20 +0100

    maj

--- Clicker/js/script-clicker.js
+++ Clicker/js/script-clicker.js
@@ -1,19 +1,37 @@
-let clicker = $('.clicker');
-let contScore = $('.cont_score');
-let score = 0;
-
-let multiplicateur = 1;
-
-let augmenterMultiplicateur = () =>{
-    
-}
-
-
-
-
-
-
-clicker.click(()=>{
-    score++;
+// Variables
+let clicker = $('#click'); // Bouton principal
+let clickerValue = 1; // Valeur du click
+let contScore = $('.cont_score'); // Div du score
+let multi = $('#multiplier') // Bouton +1
+let multiplicateur = 1; // Valeur du bouton +1
+let score = 0; // Score
+// Fonctions
+let afficheScore = () =>{ // Affiche le score
     contScore.html(score);
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Bouton principale
+clicker.click(()=>{
+    score = score + (clickerValue * multiplicateur);
+    afficheScore();
+    
+})
+
+// Bouton +1
+multi.click(()=>{
+    multiplicateur++;
+    console.log(multiplicateur)
 })
