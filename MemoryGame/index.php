8df98ffd4056968895412c43e0818f76fafddb6e
Canivet Geoffrey <geoffreycanivet@gmail.com>
2018-11-20 20:17:43 +0100

    maj

--- MemoryGame/index.php
+++ MemoryGame/index.php
@@ -1,1 +1,72 @@
-<<i class="fas fa-h1    ">Memory Game</i>
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css"  />
+    <title>Memory Game</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+            <table class="">
+            <tbody>
+                <tr>
+                    <td class="carte"></td>
+                    <td class="carte"></td>
+                    <td class="carte"></td>
+                    <td class="carte"></td>
+                </tr>
+                <tr>
+                    <td class="carte"></td>
+                    <td class="carte"></td>
+                    <td class="carte"></td>
+                    <td class="carte"></td>
+                </tr>
+                <tr>
+                    <td class="carte"></td>
+                    <td class="carte"></td>
+                    <td class="carte"></td>
+                    <td class="carte"></td>
+                </tr>
+                <tr>
+                    <td class="carte"></td>
+                    <td class="carte"></td>
+                    <td class="carte"></td>
+                    <td class="carte"></td>
+                </tr>
+            </tbody>
+        </table>
+            </div>
+            <div class="col">
+                <div class="right">
+                <h1>Memory Game</h1>
+        <div class="cont-chrono">
+            <h2>60</h2>
+        </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero necessitatibus itaque error officia, voluptates modi voluptatum eligendi similique, recusandae iste laboriosam doloremque atque, voluptatibus iusto? Velit illo temporibus sapiente numquam?</p>
+                    <button type="button" class="btn btn-color">START</button>
+                </div>
+            </div>
+        </div>
+        
+        
+        
+    </div>
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
